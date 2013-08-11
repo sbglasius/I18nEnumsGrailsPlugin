19beea9ef57dd6a3448e22e6dfa95b613ae8a684
Burt Beckwith <burt@burtbeckwith.com>
2013-08-11 12:21:27 -0400

    dropped minimum Grails version to 2.0

--- EnumMessageSourceResolvableGrailsPlugin.groovy
+++ EnumMessageSourceResolvableGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class EnumMessageSourceResolvableGrailsPlugin {
 	// the plugin version
 	def version = "0.9"
 	// the version or versions of Grails the plugin is designed for
-	def grailsVersion = "2.2 > *"
+	def grailsVersion = "2.0 > *"
 	// the other plugins this plugin depends on
 	def dependsOn = [:]
 	// resources that are excluded from plugin packaging

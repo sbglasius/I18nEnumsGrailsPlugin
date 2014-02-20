b9ea15430d8be52facb090e56fa7526e8112c06c
Soeren Berg Glasius <soeren@glasius.dk>
2014-02-20 11:40:46 +0100

    Test project (does not test correctly)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -4,4 +4,3 @@ log4j = {
 	      'org.hibernate',
 	      'net.sf.ehcache.hibernate'
 }
-
---
+++ test/project/I18nEnumsTest/src/groovy/test/TestEnum.groovy
@@ -0,0 +1,12 @@
+package test
+
+import grails.plugin.i18nEnums.annotations.I18nEnum
+
+/**
+ * Created by sbg on 20/02/14.
+ */
+@I18nEnum
+public enum TestEnum {
+
+    ONE
+}

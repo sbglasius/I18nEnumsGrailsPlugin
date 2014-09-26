85c982c12873c4fb3cf52f69efa7564d68883299
Soeren Berg Glasius <soeren@glasius.dk>
2014-09-26 10:06:37 -0700

    Update to plugins to allow publishing the plugin (this requires a version bump)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,15 +13,10 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
 	}
 
 	plugins {
-		build(':release:2.2.1', ':rest-client-builder:1.0.3') {
-			export = false
-		}
-		test(":spock:0.7") {
-			exclude "spock-grails-support"
+		build(':release:3.0.1', ':rest-client-builder:2.0.4-SNAPSHOT') {
 			export = false
 		}
 	}

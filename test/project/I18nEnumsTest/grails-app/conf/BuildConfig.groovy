575e39df0ecb3f0517fafdf67f50fafc0c06e468
Soeren Berg Glasius <soeren@glasius.dk>
2014-09-29 15:44:11 -0700

    Upgrade plugins in test-project

--- test/project/I18nEnumsTest/grails-app/conf/BuildConfig.groovy
+++ test/project/I18nEnumsTest/grails-app/conf/BuildConfig.groovy
@@ -34,43 +34,26 @@ grails.project.dependency.resolution = {
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
 
         grailsPlugins()
         grailsHome()
         mavenLocal()
         grailsCentral()
         mavenCentral()
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-        // runtime 'mysql:mysql-connector-java:5.1.27'
-        // runtime 'org.postgresql:postgresql:9.3-1100-jdbc41'
-        compile 'org.grails.plugins:i18n-enums:1.0.2-SNAPSHOT'
     }
 
     plugins {
-        // plugins for the build system only
-        build ":tomcat:7.0.50"
+        build ":tomcat:7.0.54"
 
-        // plugins for the compile step
-        compile ":scaffolding:2.0.1"
-        compile ':cache:1.1.1'
+        compile ":scaffolding:2.1.2"
+        compile ':cache:1.1.8'
 
-        // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.7" // or ":hibernate4:4.1.11.6"
-        runtime ":database-migration:1.3.8"
-        runtime ":jquery:1.10.2.2"
-        runtime ":resources:1.2.1"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
+        compile 'org.grails.plugins:i18n-enums:1.0.7'
+
+        runtime ":hibernate:3.6.10.17" // or ":hibernate4:4.1.11.6"
     }
 }
 
